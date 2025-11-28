07de159f1811abc695b447e022c697361c83d046
Marcus <marcus.ribeiro0104@gmail.com>
2025-11-28 15:19:26 -0300

    caixa_loteria.php

--- caixa_loteria.php
+++ caixa_loteria.php
@@ -1,4 +1,25 @@
 <?php
+print ("
+    \033[1;097m------------------------
+    |    loteria caixa      |
+    -------------------------
+    |Qual jogo deseja jogar:|
+    -------------------------
+    |\033[1;092m1° Mega-sena\033[097m           |   
+    -------------------------
+    |\033[1;094m2° Quina\033[097m               |
+    -------------------------
+    |\033[1;095m3° Lotofácil\033[097m           |
+    -------------------------
+    |\033[1;093m4° Lotomania\033[097m           | 
+    -------------------------\033[0m\n");
+do {
+    $loteria = (int) readline();
+    if ($loteria < 1 or $loteria> 4){
+        print ("\033[1;091mOpção invalida!!\033[0m\n");
+        print ("\033[1;096mTente novamente\033[0m\n");
+    }else{
+    switch ($loteria) {<?php
 print("
     ------------------------
     |    loteria caixa     |
@@ -62,13 +83,64 @@ do {
 } while ($loteria > 4 or $loteria < 1);
 
 print "\nQuantas apostas você deseja gerar: \n";
+
+        case 1:
+            print ("\033c");
+            print ("\033[1;092mMega-Sena selecionada: \033[0m");
+
+            $min = 6;
+            $max = 20;
+            $preco = 6.00;
+            $intervalo = [1, 60];
+
+            break;
+        case 2:
+            print ("\033c");
+            print ("\033[1;095mLotofácil selecionada: \033[0m");
+            $min = 15;
+            $max = 20;
+            $preco = 3.50;
+            $intervalo = [1, 25];
+            break;
+        case 3:
+            print ("\033c");
+            print ("\033[1;094mQuina selecionada: \033[0m");
+            $min = 5;
+            $max = 15;
+            $preco = 3.00;
+            $intervalo = [1, 80];
+
+            break;
+        case 4:
+            print ("\033c");
+            print ("\033[1;093mLotomanía selecionada: \033[0m");
+            $min = 50;
+            $max = 50;
+            $preco = 3.00;
+            $intervalo = [1, 100];
+
+            break;
+        }
+    }
+} while ($loteria > 4 or $loteria < 1);
+
+print ("\n\033[1;097mQuantas apostas você deseja gerar: \033[0m\n");
 do {
-    $quantidade_apostas = (int) readline();
+   $quantidade_apostas = (int) readline();
+   if ($quantidade_apostas < 1){
+        print ("\033[1;091mOpção invalida!!\033[0m\n");
+        print ("\033[1;096mNúmero de apostas mínimas é 1\033[0m\n");
+        print ("\033[1;096mTente novamente\033[0m\n");      
+   } 
 } while ($quantidade_apostas < 1);
 
-print "Quantas dezenas você quer jogar: (Minimo: $min. Máximo $max) \n";
+print ("\033[1;097mQuantas dezenas você quer jogar: (Mínimo: $min. Máximo $max)\033[0m\n");
 do {
     $quantidade_dezenas = (int) readline();
+    if ($quantidade_dezenas< $min or $quantidade_dezenas > $max){
+        print ("\033[1;091mOpção invalida!!\033[0m\n");
+        print ("\033[1;096mTente novamente\033[0m\n");  
+    }
 } while ($quantidade_dezenas < $min or $quantidade_dezenas > $max);
 
 $n = $quantidade_dezenas;
@@ -89,14 +161,12 @@ for ($i = $nk; $i >= 1; $i--) {
     $fatorial_nk *= $i;
 }
 $preco = ($fatorial_n / ($fatorial_k * $fatorial_nk)) * $preco;
-print "\033c";
-print "Valor total: R$" . $quantidade_apostas * $preco . "\nPreço por aposta: R$$preco\n";
+print ("\033c");
+print ("\033[1;097mValor total: R$" . $quantidade_apostas * $preco . "\nPreço por aposta: R$$preco\n");
 
 for ($i = 0; $i < $quantidade_apostas; $i++) {
-    print($i+1 . "º aposta: " . implode(", ",surpresinha($quantidade_dezenas, $intervalo[0], $intervalo[1])) . "\n");
+    print ($i+1 . "º aposta: " . implode(", ",surpresinha($quantidade_dezenas, $intervalo[0], $intervalo[1])) . "\n");
 }
-
-
 
 function surpresinha($dezenas, $min, $max)
 {
